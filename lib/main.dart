f3925d1cfad8ef60200dc25af965f1afadf1294c
Bishoy <bishoyabdo236@gmail.com>
2023-11-19 17:27:33 +0200

    a new version with animation option to restart and start select

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+  const MyHomePage({Key? key});
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -29,9 +29,10 @@ class _MyHomePageState extends State<MyHomePage>
     with SingleTickerProviderStateMixin {
   late AnimationController animationController;
   late Animation animation;
-  List<Offset> list = [];
-  List<Color> colorList = [];
-  int selectedCircleIndex = -1;
+  Map<int, Offset?> pointerPositions = {};
+  Map<int, Color> pointerColors = {};
+  int? selectedCircleIndex;
+  bool animationInProgress = false;
 
   @override
   void initState() {
@@ -47,104 +48,131 @@ class _MyHomePageState extends State<MyHomePage>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            // Reset the state variables
-            list.clear();
-            colorList.clear();
-            selectedCircleIndex = -1;
-
-            // Stop the animation
-            animationController.reset();
-          });
-        },
-        child: const Icon(Icons.refresh),
-      ),
       appBar: AppBar(
-        title: const Text('Drag Position Example'),
+        title: const Text('Choose One'),
       ),
-      body: GestureDetector(
-        onPanStart: (DragStartDetails details) {
-          setState(() {
-            list.add(details.localPosition);
-            var ix = Color(Random().nextInt(0xFFFFFFFF)).withOpacity(1.0);
-            colorList.add(ix);
-          });
-        },
-        onPanEnd: (DragEndDetails details) {
-          Timer(const Duration(seconds: 10), () {
-            setState(() {
-              // Keep a random circle and remove the rest
-              if (list.isNotEmpty) {
-                final randomIndex = Random().nextInt(list.length);
-                selectedCircleIndex = randomIndex;
-                animationController.repeat();
-              }
-            });
-          });
-        },
-        child: Container(
-          width: double.infinity,
-          height: double.infinity,
-          color: Colors.white,
-          child: Stack(
-            children: [
-              // Use a loop to create circular containers for each position in the list
-              for (int i = 0; i < list.length; i++)
-                Positioned(
-                  left: list[i].dx - 25, // Adjust as needed
-                  top: list[i].dy - 25, // Adjust as needed
-                  child: Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      // Larger circle around the chosen one
-                      if (i == selectedCircleIndex)
-                        CircularRevealAnimation(
-                          animation: animationController,
-                          centerOffset: const Offset(0, 0), // Adjust as needed
-                          // Adjust as needed
-                          child: Container(
-                            width: 80,
-                            height: 80,
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              border: Border.all(
-                                color: colorList[i],
-                                width: 5,
+      body: Column(
+        children: [
+          Expanded(
+            child: Listener(
+              onPointerDown: (PointerDownEvent event) {
+                setState(() {
+                  int pointerId = event.pointer;
+                  pointerPositions[pointerId] = event.localPosition;
+                  pointerColors[pointerId] =
+                      Color(Random().nextInt(0xFFFFFFFF)).withOpacity(1.0);
+                });
+              },
+              onPointerMove: (PointerMoveEvent event) {
+                setState(() {
+                  int pointerId = event.pointer;
+                  pointerPositions[pointerId] = event.localPosition;
+                });
+              },
+              onPointerUp: (PointerUpEvent event) {
+                // The animation and position selection will only occur when the button is pressed
+              },
+              child: Container(
+                width: double.infinity,
+                height: double.infinity,
+                color: Colors.white,
+                child: Stack(
+                  children: [
+                    // Use a loop to create circular containers for each position in the list
+                    for (MapEntry<int, Offset?> entry
+                    in pointerPositions.entries)
+                      if (entry.value != null)
+                        Positioned(
+                          left: entry.value!.dx - 25, // Adjust as needed
+                          top: entry.value!.dy - 25, // Adjust as needed
+                          child: Stack(
+                            alignment: Alignment.center,
+                            children: [
+                              // Larger circle around the chosen one
+                              if (entry.key == selectedCircleIndex)
+                                CircularRevealAnimation(
+                                  animation: animationController,
+                                  centerOffset: const Offset(0, 0),
+                                  // Adjust as needed
+                                  child: Container(
+                                    width: 80,
+                                    height: 80,
+                                    decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      border: Border.all(
+                                        color: pointerColors[entry.key]!,
+                                        width: 5,
+                                      ),
+                                    ),
+                                  ),
+                                )
+                              else
+                                Container(
+                                  width: 60,
+                                  height: 60,
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    border: Border.all(
+                                      color: pointerColors[entry.key]!,
+                                      width: 5,
+                                    ),
+                                  ),
+                                ),
+                              Container(
+                                width: 40,
+                                height: 40,
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  border: Border.all(
+                                    color: pointerColors[entry.key]!,
+                                    width: 4,
+                                  ),
+                                ),
                               ),
-                            ),
-                          ),
-                        )
-                      else
-                        Container(
-                          width: 60,
-                          height: 60,
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            border: Border.all(
-                              color: colorList[i],
-                              width: 5,
-                            ),
+                            ],
                           ),
                         ),
-                      Container(
-                        width: 40,
-                        height: 40,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          border: Border.all(
-                            color: colorList[i],
-                            width: 4,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+                  ],
                 ),
+              ),
+            ),
+          ),
+          // Button Row
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconButton(
+                onPressed: () {
+                  // Choose a random circle
+                  setState(() {
+                    selectedCircleIndex =
+                        pointerPositions.keys.elementAt(Random().nextInt(pointerPositions.length));
+                    animationInProgress = true;
+                    // Start the animation
+                    animationController.repeat();
+                  });
+                },
+                icon: const Icon(Icons.shuffle),
+              ),
+              FloatingActionButton(
+                onPressed: () {
+                  setState(() {
+                    // Reset the state variables
+                    pointerPositions.clear();
+                    pointerColors.clear();
+                    selectedCircleIndex = null;
+                    animationInProgress = false;
+
+                    // Stop and reset the animation
+                    animationController.stop();
+                    animationController.reset();
+                  });
+                },
+                child: const Icon(Icons.refresh),
+              ),
             ],
           ),
-        ),
+        ],
       ),
     );
   }
